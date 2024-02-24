36032718e6a752f92201139b345c8a73cedbd7fb
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-02-25 02:46:36 +0400

    holberton

--- markdown2html.py
+++ markdown2html.py
@@ -19,7 +19,7 @@ def convert_markdown_to_html(md_file, html_file):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py <Markdown file> <HTML output file>", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
     md_file = sys.argv[1]
