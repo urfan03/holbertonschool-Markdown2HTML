4aacbdf40598e3e669f27967f1f0a82a2e03b16f
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-02-25 12:41:41 +0400

    holberton

--- markdown2html.py
+++ markdown2html.py
@@ -1,41 +1,109 @@
 #!/usr/bin/python3
-"""Convert Markdown to HTML"""
+""" Write a script markdown2html.py that takes an argument 2 strings:
+
+First argument is the name of the Markdown file
+Second argument is the output file name """
 
 import sys
 import os
-import markdown
+import hashlib
+import re
 
-def convert_markdown_to_html(md_file, html_file):
-    """
-    Convert Markdown file to HTML file.
+def To_HTML(name,text):
+    return "<"+name+">"+text+"</"+name+">\n"
 
-    Args:
-        md_file (str): Path to the Markdown file.
-        html_file (str): Path to the HTML output file.
+def Heading(line):    
+    count = line.count('#')
+    name = "H"+str(count)
+    return To_HTML(name,line[count+1:])
 
-    Raises:
-        FileNotFoundError: If the Markdown file does not exist.
-    """
-    if not os.path.exists(md_file):
-        sys.stderr.write(f"Missing {md_file}\n")
-        sys.exit(1)
+def UO_List(lines,name):
+    text = ""
+    for line in lines:
+        text += "\t" + To_HTML("li",line[2:])
+    return To_HTML(name,"\n" + text)
 
-    with open(md_file, 'r') as md:
-        markdown_text = md.read()
+def Paragraph(lines):
+    text = ""
+    for line in lines:
+        if(line!=lines[0]):
+            text +='<br/>'
+        text += '\n\t' + line + '\n'  
+    return To_HTML('p',text)
 
-    html_text = markdown.markdown(markdown_text)
+def GetLines(lines,char = '', IsAlpha = False):
+    L = [Bold(Em(SQ_brackets(Brackets(lines[0]))))]
+    lines.remove(lines[0])
+    while(len(lines) != 0):
+        line = lines[0]
+        if(line==''):
+            break
+        if((line[0].isalpha and IsAlpha) or line[0] == char):
+            lines.remove(line)
+            line = Bold(Em(SQ_brackets(Brackets(line))))
+            L.append(line)
+        else: 
+            break
+    return L
 
-    with open(html_file, 'w') as html:
-        html.write(html_text)
+def Bold(line):
+    line = re.sub("\*\*","<b>",line,1)
+    line = re.sub("\*\*","</b>",line,1)
+    return line
 
+def Em(line):
+    line = re.sub("__","<em>",line,1)
+    line = re.sub("__","</em>",line,1)
+    return line
+
+def SQ_brackets(line):
+    id = line.find("[[")
+    id2 = line.find("]]")
+    if(id!=-1 and id2!=-1):
+        text = line[id+2:id2]
+        line = line[:id] + str(hashlib.md5(text.encode()).hexdigest())+ line[id2+2:]
+    return line
+
+def Brackets(line):
+    id = line.find("((")
+    id2 = line.find("))")
+    if(id!=-1 and id2!=-1):
+        text = line[id+2:id2]
+        text = text.replace('c','')
+        text = text.replace('C','')
+        line = line[:id] + text + line[id2+2:]
+    return line
+    
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
-
-    md_file = sys.argv[1]
-    html_file = sys.argv[2]
-
-    convert_markdown_to_html(md_file, html_file)
-
-    sys.exit(0)
+        exit(1)
+    if not os.path.exists(sys.argv[1]):
+        sys.stderr.write("Missing " + sys.argv[1] + "\n")
+        exit(1)
+    with open(sys.argv[1]) as IN:
+        with open(sys.argv[2], 'w') as OUT:
+            lines = IN.read().splitlines()
+            html = ""
+            
+            while(len(lines) != 0):
+                line = lines[0]
+                if(line == ""):
+                    lines.remove(line)
+                else:
+                    line = Bold(Em(SQ_brackets(Brackets(line))))
+                    
+                    if(line[0]=="#"):
+                        html += Heading(line)
+                        del lines[0]
+                    elif(line[0]=='-'):
+                        L = GetLines(lines,'-')
+                        html += UO_List(L,"ul")        
+                    elif(line[0]=='*'):
+                        L = GetLines(lines,'*')
+                        html += UO_List(L,"ol")
+                    elif(line[0].isalpha):
+                        L = GetLines(lines, IsAlpha = True)
+                        html += Paragraph(L)
+            OUT.write(html)
+    exit(0)
