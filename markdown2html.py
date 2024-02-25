6f5a1f8501fe98aaa80ef6a6dc948efcbe9064b1
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-02-25 04:14:01 +0400

    holberton

--- markdown2html.py
+++ markdown2html.py
@@ -1,56 +1,29 @@
 #!/usr/bin/python3
-'''This script takes two arguments: first is a markdown file name and the
-    second is an html output filename
-'''
+''' Markdown to HTML '''
+import sys
+from os import path
 
 
-if __name__ == '__main__':
-    import sys
-    import os
-    if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
-
-    if not os.path.isfile(sys.argv[1]):
+if __name__ == "__main__":
+    '''the number of arguments is less than 2'''
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html' ,file=sys.stderr)
+        exit(1)
+    '''Markdown file doesn’t exist'''
+    if not path.exists(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-        sys.exit(1)
-
-    sym_map = {
-            '# ': ('<h1>', '</h1>\n'),
-            '## ': ('<h2>', '</h2>\n'),
-            '### ': ('<h3>', '</h3>\n'),
-            '#### ': ('<h4>', '</h4>\n'),
-            '##### ': ('<h5>', '</h5>\n'),
-            '###### ': ('<h6>', '</h6>\n')
-        }
-    ul = []
-
-    with open(sys.argv[1]) as rf:
-        read_lines = rf.readlines()
-        with open(sys.argv[2], 'a') as wf:
-            for line in read_lines:
-                if line.startswith('- '):
-                    ul.append(line)
-
-                if ul:
-                    i = 0
-                    while i < len(ul):
-                        if i == 0:
-                            html_line = ul[i].replace('- ', '<ul>\n<li>').replace(ul[i][-1], '</li>\n')
-                        else:
-                            html_line = ul[i].replace('- ', '<li>').replace('\n', '</li>\n')
-                        if i == len(ul) - 1:
-                            html_line = html_line.replace(html_line[-1], '\n</ul>\n')
-                            wf.write(html_line)
-                        else:
-                            wf.write(html_line)
-                        i = i + 1
-
-                for k, v in sym_map.items():
-                    if line.startswith(k):
-                        html_line = line.replace(k, v[0]).replace('\n', v[1])
-                        break
-                wf.write(html_line)
-
-    sys.exit(0)
+        exit(1)
+    '''Headings Markdown'''
+    with open(sys.argv[1], 'r') as read_file:
+        line_list = []
+        for lines in read_file.readlines():
+            cout_cra = 0
+            for line in lines:
+                for car in range(len(line)):
+                    if line[car] == '#':
+                        cout_cra += 1
+            lines = lines.rstrip('\r\n')
+            line_list.append("<h{}>{}</h{}>".format(cout_cra, lines.replace('#',''), cout_cra))
+        with open(sys.argv[2], 'a') as write_file:
+            for line in line_list:
+                write_file.write('{}\n'.format(line))
