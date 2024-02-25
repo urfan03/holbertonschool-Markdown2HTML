b4283b501195a6c3eafd8128766bc5c23304043a
Urfan Musayev <83137111+urfan03@users.noreply.github.com>
2024-02-25 04:12:54 +0400

    holberton

--- markdown2html.py
+++ markdown2html.py
@@ -1,32 +1,56 @@
 #!/usr/bin/python3
-import sys
-import os
-import markdown
+'''This script takes two arguments: first is a markdown file name and the
+    second is an html output filename
+'''
 
 
-def convert_markdown_to_html(md_file, html_file):
-
-    if not os.path.exists(md_file):
-        print(f"Missing {md_file}", file=sys.stderr)
-        sys.exit(1)
-
-    with open(md_file, 'r') as f:
-        markdown_text = f.read()
-
-    html_text = markdown.markdown(markdown_text)
-
-    with open(html_file, 'w') as f:
-        f.write(html_text)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import sys
+    import os
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         sys.exit(1)
 
-    md_file = sys.argv[1]
-    html_file = sys.argv[2]
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        sys.exit(1)
 
-    convert_markdown_to_html(md_file, html_file)
+    sym_map = {
+            '# ': ('<h1>', '</h1>\n'),
+            '## ': ('<h2>', '</h2>\n'),
+            '### ': ('<h3>', '</h3>\n'),
+            '#### ': ('<h4>', '</h4>\n'),
+            '##### ': ('<h5>', '</h5>\n'),
+            '###### ': ('<h6>', '</h6>\n')
+        }
+    ul = []
+
+    with open(sys.argv[1]) as rf:
+        read_lines = rf.readlines()
+        with open(sys.argv[2], 'a') as wf:
+            for line in read_lines:
+                if line.startswith('- '):
+                    ul.append(line)
+
+                if ul:
+                    i = 0
+                    while i < len(ul):
+                        if i == 0:
+                            html_line = ul[i].replace('- ', '<ul>\n<li>').replace(ul[i][-1], '</li>\n')
+                        else:
+                            html_line = ul[i].replace('- ', '<li>').replace('\n', '</li>\n')
+                        if i == len(ul) - 1:
+                            html_line = html_line.replace(html_line[-1], '\n</ul>\n')
+                            wf.write(html_line)
+                        else:
+                            wf.write(html_line)
+                        i = i + 1
+
+                for k, v in sym_map.items():
+                    if line.startswith(k):
+                        html_line = line.replace(k, v[0]).replace('\n', v[1])
+                        break
+                wf.write(html_line)
 
     sys.exit(0)
